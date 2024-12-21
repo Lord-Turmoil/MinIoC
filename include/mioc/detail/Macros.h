f72567427c33e42c59b0852d83e66f7745b8f8f0
Lord Turmoil <swift-phantom@outlook.com>
2024-12-21 20:45:08 +0800

    feat: to header only library

--- include/mioc/detail/Macros.h
+++ include/mioc/detail/Macros.h
@@ -11,8 +11,8 @@
 constexpr bool DEFAULT_LAZINESS = true;
 
 #define MIOC_VERSION_MAJOR 1
-#define MIOC_VERSION_MINOR 2
-#define MIOC_VERSION_PATCH 3
+#define MIOC_VERSION_MINOR 3
+#define MIOC_VERSION_PATCH 0
 
 #define MIOC_TO_VERSION(major, minor, patch) (major * 10000 + minor * 100 + patch)
 #define MIOC_VERSION                         MINET_TO_VERSION(DGEX_VERSION_MAJOR, DGEX_VERSION_MINOR, DGEX_VERSION_PATCH)
